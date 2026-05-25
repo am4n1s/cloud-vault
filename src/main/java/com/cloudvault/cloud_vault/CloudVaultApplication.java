5533219f193ba7ffd1eceb26253c9c81e8b67593
am4n1s <aman1s.exe@gmail.com>
2026-05-25 14:22:50 +0500

    feat: initial project structure from spring initializr

---
+++ src/main/java/com/cloudvault/cloud_vault/CloudVaultApplication.java
@@ -0,0 +1,13 @@
+package com.cloudvault.cloud_vault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudVaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloudvault/cloud_vault/CloudVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudvault.cloud_vault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
